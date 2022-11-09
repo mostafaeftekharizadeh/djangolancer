151cc901a2fe1a23ba11d4dec11004eac921806a
Mostafa Eftekhari Zadeh <mostafa.eftekhari@gmail.com>
2022-11-10 00:00:44 +0330

    remove api/ from api path

--- configuration/views.py
+++ configuration/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from rest_framework import authentication, permissions
 from django.shortcuts import render
+from library.permissions import IsAdminOrReadOnly
 from .models import (Estimate,
                      ProfileType,
                      Bank,
@@ -29,65 +30,65 @@ from .serializers import (EstimateSerializer,
 class EstimateViewSet(viewsets.ModelViewSet):
     queryset = Estimate.objects.all()
     serializer_class = EstimateSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsAdminOrReadOnly]
 
 class ProfileTypeViewSet(viewsets.ModelViewSet):
     queryset = ProfileType.objects.all()
     serializer_class = ProfileTypeSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsAdminOrReadOnly]
 
 class BankViewSet(viewsets.ModelViewSet):
     queryset = Bank.objects.all()
     serializer_class = BankSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsAdminOrReadOnly]
 
 class LanguageViewSet(viewsets.ModelViewSet):
     queryset = Language.objects.all()
     serializer_class = BaseLanguageSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsAdminOrReadOnly]
 
 class LevelViewSet(viewsets.ModelViewSet):
     queryset = Level.objects.all()
     serializer_class = BaseLevelSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsAdminOrReadOnly]
 
 class ViewStatusViewSet(viewsets.ModelViewSet):
     queryset = ViewStatus.objects.all()
     serializer_class = ViewStatusSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsAdminOrReadOnly]
 
 class CurrencyViewSet(viewsets.ModelViewSet):
     queryset = Currency.objects.all()
     serializer_class = CurrencySerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsAdminOrReadOnly]
 
 class StatusViewSet(viewsets.ModelViewSet):
     queryset = Status.objects.all()
     serializer_class = StatusSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsAdminOrReadOnly]
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsAdminOrReadOnly]
 
 class SkillViewSet(viewsets.ModelViewSet):
     queryset = Skill.objects.all()
     serializer_class = BaseSkillSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsAdminOrReadOnly]
 
 class ComplainTypeViewSet(viewsets.ModelViewSet):
     queryset = ComplainType.objects.all()
     serializer_class = ComplainTypeSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsAdminOrReadOnly]
 
 class DegreeViewSet(viewsets.ModelViewSet):
     queryset = Degree.objects.all()
     serializer_class = DegreeSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsAdminOrReadOnly]
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsAdminOrReadOnly]
 
