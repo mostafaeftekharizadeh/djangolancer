c2667f97a7bc53353a05faa8c1bbc31cdf3de5ae
Mostafa Eftekhari Zadeh <mostafa.eftekhari@gmail.com>
2022-11-10 00:00:59 +0330

    refactoring

--- user/views.py
+++ user/views.py
@@ -9,6 +9,7 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from django_filters import rest_framework as filters
+from library.permissions import IsOwnerOrReadOnly
 from .serializers import PartySerializer, UserSerializer
 from .serializers import ChangePasswordSerializer
 from .serializers import UpdateUserSerializer
