1c0bb1023c3b30aff69ea891d7c4031e6e9dbdee
Mostafa Eftekhari Zadeh <mostafa.eftekhari@gmail.com>
2022-11-10 00:18:12 +0330

    add new IsAdminOrAuthenticated permission

--- library/permissions.py
+++ library/permissions.py
@@ -1,6 +1,6 @@
 from rest_framework import permissions
 
-
+# only authenticated user add, only owner can delete/put, AnyOne can read
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
@@ -12,9 +12,17 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
             return True
         return obj.party.user == request.user
 
+# only staff users can add/delete/put, AnyOne can read
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
+        return request.user.is_staff
+
+# only staff users can add/delete/put, authenticated user can read
+class IsAdminOrAuthenticated(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return request.user.is_authenticated
         return request.user.is_staff
 
