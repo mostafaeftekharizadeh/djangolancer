eda8d08fd0a9da5865450a14efb5ec9414826c3f
Mostafa Eftekhari Zadeh <mostafa.eftekhari@gmail.com>
2022-11-10 13:39:41 +0330

    .

--- midlancer/settings.py
+++ midlancer/settings.py
@@ -89,18 +89,18 @@ WSGI_APPLICATION = 'midlancer.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
-    #}
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'midlancer',
-        'USER': 'root',
-        'PASSWORD': '1',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.mysql',
+    #    'NAME': 'midlancer',
+    #    'USER': 'root',
+    #    'PASSWORD': '1',
+    #    'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+    #    'PORT': '3306',
+    #}
 }
 
 
