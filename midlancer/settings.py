9af95591e320029a08426afda7efa14a7665bac1
Mostafa Eftekhari Zadeh <mostafa.eftekhari@gmail.com>
2022-12-13 00:17:22 +0330

    set default drf pagination to 10

--- midlancer/settings.py
+++ midlancer/settings.py
@@ -153,7 +153,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 1,
+    'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
