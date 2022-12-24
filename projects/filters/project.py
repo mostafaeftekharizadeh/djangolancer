5f19473e129a4affb712381df2e04001430ebdc6
Mostafa Eftekhari Zadeh <mostafa.eftekhari@gmail.com>
2022-12-24 09:47:18 +0330

    add search to project api

--- projects/filters/project.py
+++ projects/filters/project.py
@@ -7,6 +7,7 @@ from projects.models import Project
 
 class ProjectFilter(FilterSet):
     owner = filters.CharFilter(method='owner_filter')
+    q = filters.CharFilter(method='owner_filter')
     skill = CharInFilter(field_name="skill__name", lookup_expr='in')
     level = CharInFilter(field_name="level__name", lookup_expr='in')
     class Meta:
@@ -17,15 +18,23 @@ class ProjectFilter(FilterSet):
         return queryset
     @property
     def qs(self):
-        owner = self.request.query_params.get( 'owner', None)
+        owner = self.request.query_params.get('owner', None)
+        q = self.request.query_params.get('q', None)
         user = getattr(self.request, 'user', None)
-        parent = super().qs
+        qs = super().qs
         if user and user.is_authenticated:
             if owner:
-                return parent.filter(Q(party__user=user))
+                qs =  qs.filter(Q(party__user=user))
             else:
-                return parent.filter(~Q(party__user=user))
-        else:
-            return parent
+                qs = qs.filter(~Q(party__user=user))
+
+        if q:
+            qs =  qs.filter(Q(skill__name__icontains=q)|
+                            Q(level__name=q)|
+                            Q(category__name=q)|
+                            Q(sub_category__name=q)|
+                            Q(title__icontains=q)
+                            )
+        return qs
 
 
