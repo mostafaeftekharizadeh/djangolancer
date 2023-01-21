5b7faaed3b6e41974da8c061216ce3f93a78f80b
Mostafa Eftekhari Zadeh <mostafa.eftekhari@gmail.com>
2023-01-21 23:39:20 +0330

    midlancer url refactoring

--- backend/midlancer/urls.py
+++ backend/midlancer/urls.py
@@ -1,15 +1,16 @@
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+'''
+midlancer main urls
+'''
+import importlib
 from django.urls import include, path, re_path
 from django.conf.urls.static import static
 from django.contrib import admin
-from rest_framework import routers
-from rest_framework import permissions
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from rest_framework import routers, permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from django.urls import re_path
 
 from . import settings
-import importlib
 
 
 
@@ -32,9 +33,15 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0),
+            name='schema-json'),
+    re_path(r'^swagger/$',
+            schema_view.with_ui('swagger', cache_timeout=0),
+            name='schema-swagger-ui'),
+    re_path(r'^redoc/$',
+            schema_view.with_ui('redoc', cache_timeout=0),
+            name='schema-redoc'),
     path('admin/', admin.site.urls),
 ]
 
