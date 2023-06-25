2e3afcdd9b7f2498eb2dcd15146794ada95cda0c
Ali Reza Salimzadeh <salimzadeh@gmail.com>
2023-06-25 17:52:51 +0330

    fix

--- backend/midlancer/settings.py
+++ backend/midlancer/settings.py
@@ -202,14 +202,27 @@ AUTH_PROFILE_MODULE = "user.Profile"
 #         "http://127.0.0.1:3000",  # type: ignore
 #     ],
 # )
-CORS_ALLOWED_ORIGINS = os.environ.get(
-    "CORS_ALLOWED_ORIGINS",
-    default=[
-        "http://localhost:8000",  # type: ignore
-        "http://127.0.0.1:8000",  # type: ignore
-        "http://localhost:3000",  # type: ignore
-        "http://127.0.0.1:3000",  # type: ignore
-    ],
+CORS_ALLOWED_ORIGINS = list(
+    os.environ.get(
+        "CORS_ALLOWED_ORIGINS",
+        default=[
+            "http://localhost:8000",  # type: ignore
+            "http://127.0.0.1:8000",  # type: ignore
+            "http://localhost:3000",  # type: ignore
+            "http://127.0.0.1:3000",  # type: ignore
+        ],
+    )
+)
+CSRF_TRUSTED_ORIGINS = list(
+    os.environ.get(
+        "CORS_ALLOWED_ORIGINS",
+        default=[
+            "http://localhost:8000",  # type: ignore
+            "http://127.0.0.1:8000",  # type: ignore
+            "http://localhost:3000",  # type: ignore
+            "http://127.0.0.1:3000",  # type: ignore
+        ],
+    )
 )
 
 LOGGING = {
